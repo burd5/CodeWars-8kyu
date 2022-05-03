b16966784f61e2974b12705ade9eb6bc33d059c4
burd5 <austinburdette5@gmail.com>
2022-05-03 17:44:35 -0600

    completed reverse sequence challenge using a while loop

---
+++ reverseSequence.js
@@ -0,0 +1,13 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+const reverseSeq = n => {
+    let newArray = []
+    while(n >= 1){
+      newArray.push(n);
+      n--
+    }
+    return newArray;
+  };
